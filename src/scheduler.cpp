e3f4ff5270b6c3c177fbdffb234d2301086f1a76
Oliveira223 <oliveira.phno@gmail.com>
2026-09-24 14:00:10 -0300

    Corrige valores de bloqueio revertidos indevidamente pelo merge (alinha com origin/tiago)

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -33,7 +33,7 @@ ResultadoTick avancarFila0(Scheduler &scheduler)
     // 2. Bloqueou (SYSCALL 1/2) -> vai pra lista de bloqueados esperar 3 UT completas de I/O
     else if (processo.estado == Estado::BLOQUEADO)
     {
-        scheduler.bloqueados.push_back({processo, 3});
+        scheduler.bloqueados.push_back({processo, 4});
         scheduler.fila0.pop_front();
         scheduler.quantumUsadoFila0 = 0;
     }
@@ -83,7 +83,7 @@ ResultadoTick avancarFila1(Scheduler &scheduler)
         }
         else if (entrada.processo.estado == Estado::BLOQUEADO)
         {
-            scheduler.bloqueados.push_back({entrada.processo, 3});
+            scheduler.bloqueados.push_back({entrada.processo, 4});
             fila.pop_front();
         }
         else if (entrada.quantumRestante == 0)
